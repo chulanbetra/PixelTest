d16f579f7defe30249e487bea1415ee71af24a8c
chulanbetra <chulanbetra@gmail.com>
2015-10-02 23:34:01 +0200

    - moving scripts from level to prefabs folder - creating two more sorted layers for lowest and highest rendered objects

--- Assets/Scripts/AI/AINode.cs
+++ Assets/Scripts/AI/AINode.cs
@@ -4,14 +4,6 @@ using System.Collections.Generic;
 
 public class AINode : MonoBehaviour 
 {
-	// 0 - up
-	// 1 - down
-	// 2 - left
-	// 3 - right
-	// 4 - up left
-	// 5 - up right
-	// 6 - down left
-	// 7 - down right
 	public AINode[] Neighbors = new AINode[8];
 
 	public Tile Tile
